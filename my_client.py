4b67a513620a491fafeba07c5f8e048682c45c99
eric1932 <eric1932@users.noreply.github.com>
2020-04-15 14:59:39 -0500

    Still implementing more

--- my_client.py
+++ my_client.py
@@ -1,14 +1,43 @@
+import signal
 import socket
+import sys
+
+client_socket = socket.socket()
+
+
+def KeyboardInterruptHandler(signal, frame):
+    client_socket.close()
+    exit(0)
+
 
 if __name__ == '__main__':
     port = 5555
-    client_socket = socket.socket()
     try:
         client_socket.connect(('127.0.0.1', port))
     except socket.error:
         print("connection is not available")
+        client_socket.close()
+        exit(1)
+
+    signal.signal(signal.SIGINT, KeyboardInterruptHandler)
+
+    if len(sys.argv) > 1:
+        command = " ".join(sys.argv[1:])
     else:
+        sys.stderr.write("\033[33m" + "warning: no argument is given; read from input\033[39m\n")
         command = input("container$ ")
-        # command = "help"
-        client_socket.send(command.encode('utf-8'))
+    if command == "":  # replace empty string
+        command = '\0'
+    # command = "help"
+    # send command
+    client_socket.send(command.encode('utf-8'))
+    # receive output & enter interactive mode
+    interaction = int(client_socket.recv(4).decode()[-2])
+    if not interaction:
+        while True:
+            buf = client_socket.recv(1024).decode()
+            if not buf:
+                break
+            else:
+                print(buf, end='')
         client_socket.close()
--- my_daemon.py
+++ my_daemon.py
@@ -1,47 +1,79 @@
+import os
+import signal
 import socket
 import threading
-import os
-import multiprocessing
+
+# import multiprocessing
 # import daemon
 # TODO daemon
 #
 # with daemon.DaemonContext():
 #     pass
 
+server_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+client_sockets = []
 
-def command_handler(msg):
-    print(msg, "handled by pid", os.getpid())
-    if msg == "" or msg == "help":
-        print("help to be implemented")
+
+def KeyboardInterruptHandler(signal, frame):
+    server_socket.close()
+    for csoc in client_sockets:
+        csoc.close()
+    print('stop server')
+    exit(0)
+
+
+def send(soc: socket.socket, msg, delim='\n'):
+    soc.send((str(msg) + delim).encode())
+
+
+def command_handler(soc: socket.socket, msg: str):
+    print("command (" + msg + ") is handled by a thread")
+    if msg == '\0' or msg == "help":
+        send(soc, "i=0;", delim='')
+        send(soc, "commands: help exit run ps")
+    elif msg == "run":
+        send(soc, "i=1;", delim='')
+    elif msg == "ps":
+        send(soc, "i=0;", delim='')
+        send(soc, "UUID\t\tIMAGE\tCOMMAND\tCREATED\tSTATUS")
+        for x in os.listdir("./container"):
+            if os.path.isfile(os.path.join('container', x)):
+                send(soc, x[:-4][:8])
+    else:
+        # pass
+        send(soc, "i=0;", delim='')
+        send(soc, "pass")
+    soc.close()
+    client_sockets.remove(soc)
 
 
 def main():
+    if not os.path.exists('./container'):
+        os.mkdir('./container')
+    signal.signal(signal.SIGINT, KeyboardInterruptHandler)
     # TODO file socket
     print('start server, pid=' + str(os.getpid()))
-    server_socket = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
     port = 5555
     server_socket.bind(('', port))
     server_socket.listen()
 
     while True:
         client_socket, addr = server_socket.accept()
+        client_sockets.append(client_socket)
         print('connection from', addr)
         msg = client_socket.recv(1024).decode('utf-8')
         print('close client socket')
-        client_socket.close()
 
         # single thread
         # command_handler(msg)
 
         # multiple thread
-        thread = threading.Thread(target=command_handler, args=[msg])
+        thread = threading.Thread(target=command_handler, args=[client_socket, msg])
         thread.start()
         # multiple process
         # process = multiprocessing.Process(target=command_handler, args=(msg,))
         # process.join()
-
-    print('end progress')
-    server_socket.close()
 
 
 if __name__ == '__main__':
