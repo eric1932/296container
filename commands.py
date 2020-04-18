9f88ec0a6812109f02236da4da6b15618d6aa471
eric1932 <eric1932@users.noreply.github.com>
2020-04-17 23:07:56 -0500

    Add some docs and improve rm

--- commands.py
+++ commands.py
@@ -9,15 +9,16 @@ from utils import send, send_arg, find_uuid, set_interaction, get_running_contai
 
 def main_help(soc: socket.socket):
     set_interaction(soc, False)
-    send(soc, "commands: help exit run ps", newline=True)
+    with open("docs/help.txt") as f:
+        send(soc, f.read(), newline=True)
 
 
 def ps(soc: socket.socket, args: list):
     set_interaction(soc, False)
     optlist, args = getopt.getopt(args, 'ah', ['help', 'all'])
     if ('-h', '') in optlist or ('--help', '') in optlist:
-        # TODO print help
-        pass
+        with open("docs/ps.txt") as f:
+            send(soc, f.read(), newline=True)
     else:
         flag_show_all = True if ('-a', '') in optlist or ('--all', '') in optlist else False
         running = get_running_containers()
@@ -47,15 +48,10 @@ def ps(soc: socket.socket, args: list):
 
 def run(soc: socket.socket, args: list):
     optlist, args = getopt.getopt(args, 'h', ['help'])
-    if len(optlist) == 0 and len(args) == 0 or ('--help', '') in optlist:
+    if len(optlist) == 0 and len(args) == 0 or ('--help', '') in optlist or ('-h', '') in optlist:
         set_interaction(soc, False)
-        # print usage
-        send(soc, "\"run\" requires at least 1 argument.\n"
-                  "See \"run --help\"\n"
-                  "\n"
-                  "Usage: run [UUID] IMAGE\n"
-                  "\n"
-                  "Run a command in a container. If UUID is specified, do not create a new one.\n")
+        with open("docs/run.txt") as f:
+            send(soc, f.read(), newline=True)
     elif len(args) == 1:
         set_interaction(soc, True)
         send(soc, "next")  # end of passing args
@@ -80,13 +76,16 @@ def start(soc: socket.socket, args: list):
 def rm(soc: socket.socket, args: list):
     optlist, args = getopt.getopt(args, 'h', ['help'])
     set_interaction(soc, False)
-    # TODO help page
-    uuid_to_remove = find_uuid(args[0])
-    if not uuid_to_remove:
-        send(soc, "Cannot find container: " + args[0], newline=True)
-    elif uuid_to_remove in get_running_containers():
-        send(soc, "Cannot remove a running container!", newline=True)
+    if len(optlist) == 0 and len(args) == 0 or ('--help', '') in optlist or ('-h', '') in optlist:
+        with open("docs/rm.txt") as f:
+            send(soc, f.read(), newline=True)
     else:
-        os.remove(os.path.join("container", uuid_to_remove + '.img'))
-        config.delete_record(uuid_to_remove)
-        send(soc, uuid_to_remove + " is removed", newline=True)
+        uuid_to_remove = find_uuid(args[0])
+        if not uuid_to_remove:
+            send(soc, "Cannot find container: " + args[0], newline=True)
+        elif uuid_to_remove in get_running_containers():
+            send(soc, "Cannot remove a running container!", newline=True)
+        else:
+            os.remove(os.path.join("container", uuid_to_remove + '.img'))
+            config.delete_record(uuid_to_remove)
+            send(soc, uuid_to_remove + " is removed", newline=True)
