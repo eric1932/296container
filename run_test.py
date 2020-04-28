65fec9ff0c5e7ec8ea7fcd25a4b3444f67ed872b
eric1932 <eric1932@users.noreply.github.com>
2020-04-28 17:29:33 -0500

    Improve test case

--- run_test.py
+++ run_test.py
@@ -3,6 +3,7 @@ import socket
 import unittest
 from time import sleep
 
+import config
 import run
 
 
@@ -15,12 +16,19 @@ class TestRun(unittest.TestCase):
     #     run.run(False, cmd='/bin/bash')
 
     def test_nginx(self):
+        test_id = '00000000-0000-0000-0000-000000000001'
         soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TODO nginx does not listen on IPv6
-        run.run(True, cmd=('/usr/sbin/nginx', '-g', 'daemon off;'), image='nginx.img', uuid='001')
+        run.run(True, cmd=('/usr/sbin/nginx', '-g', 'daemon off;'), image='nginx', uuid=test_id)
         sleep(1)
         result = soc.connect_ex(('localhost', 80))
+
+        # clean up
         soc.close()
-        run.terminate('001')
+        run.terminate(test_id)
+        os.remove(os.path.join("container", test_id + '.img'))
+        config.delete_record(test_id)
+
+        # check result
         self.assertEqual(result, 0, msg="Could not connect to localhost:80, return code is {0}".format(result))
         print("Nginx is running normally")
 
